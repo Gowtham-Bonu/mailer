f86f7bc4b7cabdcb43641ec86cd3a14b3a02af9a
gowtham-bonu-simform <gowtham@simformsolutions.com>
2023-05-22 17:41:04 +0530

    TE7-T130:ActionMailerBasicsAndFileUpload:Main:AddedMailerCallback

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,22 @@
 class UserMailer < ApplicationMailer
+  before_action :set_receiver
+  
   default from: "gowthambonu99@gmail.com"
 
   def welcome_email
-    @user = params[:user]
     mail(to: @user.email, subject: "welcome email!")
   end
 
   def email_changed
-    @user = params[:user]
     mail(to: @user.email, subject: "email_updated!") do |format|
       format.html { render layout: 'email_layout' }
       format.text
     end
+  end
+
+  private
+
+  def set_receiver
+    @user = params[:user]
   end
 end
